4f8e72215882e83a67901187812d6eed2800b11f
freetimezp <kraftukrnet@ukr.net>
2024-03-22 21:04:43 +0200

    05. js-telegram-bot-01. Refactor. Toggle.

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import './header.css';
 import Button from '../Button/Button';
+import { useTelegram } from '../../hooks/useTelegram';
 
 
 const Header = () => {
-    const tg = window.Telegram.WebApp;
-
-    const onClose = () => {
-        tg.close();
-    };
+    const { user, onClose } = useTelegram();
 
     return (
         <div className='header'>
             <Button onClick={onClose}>Close</Button>
             <span className={'username'}>
-                {tg.initDataUnsafe?.user?.username}
+                {user?.username}
             </span>
         </div>
     );
