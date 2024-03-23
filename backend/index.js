3f368e2e575b149f64a469d4c8ac896707428232
freetimezp <kraftukrnet@ukr.net>
2024-03-23 14:44:47 +0200

    09. js-telegram-bot-01. Express server.

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const cors = require('cors');
 
 const token = process.env.REACT_APP_TELEGRAM_BOT_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
+const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 //const webAppUrl = 'https://js-eatme-11-fruits.vercel.app/';
 const webAppUrl = 'https://js-telegram-bot-01.vercel.app/';
@@ -48,3 +54,34 @@ bot.on('message', async (msg) => {
 
 });
 
+app.post('/web-data', async (req, res) => {
+    const { queryId, products, totalPrice } = req.body;
+
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Success Buying',
+            input_message_content: {
+                message_text: 'Thank you for buying our products!'
+            }
+        });
+
+        return res.status(200).json({});
+    } catch (error) {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Fail Buying',
+            input_message_content: {
+                message_text: 'Have an error when buying our products. Try one more time..'
+            }
+        });
+
+        return res.status(500).json({});
+    }
+});
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log('server running on port 8000'));
